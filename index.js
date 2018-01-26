6530645152ee0a7e58f61f4522a1de413d7e2ed6
Aspen James <aspenjames@tqca.org>
2018-01-26 00:36:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function getFirstSelector(selector){
-  return document.querySelector(selector)
+  return document.querySelector(selector);
 }
 
 function nestedTarget(){
-  return document.getElementById('nested').querySelector('div.target')
+  return document.getElementById('nested').querySelector('div.target');
 }
 
 function deepestChild(){
   let children = [];
-  children = document.getElementById('grand-node').querySelectorAll('div') 
-  return children[children.length-1]
+  children = document.getElementById('grand-node').querySelectorAll('div'); 
+  return children[children.length-1];
 }
 
 function increaseRankBy(n){
-  
+  let ranks = [];
+  ranks = document.getElementByClassName('ranked-list').querySelectorAll('li');
+  for(let i=0;i<ranks.length;i++){
+    parseInt(ranks[i].innerText)
+  }
 }
