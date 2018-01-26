735ef397b941f982e4bc0e01dfc496e0e38c6a32
Aspen James <aspenjames@tqca.org>
2018-01-26 00:43:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function deepestChild(){
+  let children = [];
+  children = document.getElementById('grand-node').querySelectorAll('div'); 
+  return children[children.length-1];
+}
+
+function increaseRankBy(n){
+  let ranks = [];
+  ranks = document.getElementsByClassName('ranked-list');
+  for(let i=0;i<ranks.length;i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML,10) + parseInt(n,10);
+  }
+  return
+}
