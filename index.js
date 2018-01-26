7486dd9bd832a2d5a14f8242e8bf42776ca1853f
Aspen James <aspenjames@tqca.org>
2018-01-26 00:25:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementById('nested').querySelector(dev.target)
+  return document.getElementById('nested').querySelector(div.target)
 }
